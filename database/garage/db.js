3380e659623222ade0ce02060dbacdb44c040f24
sarah <slachehab6@gmail.com>
2021-06-07 17:23:24 +0200

    Etape5 Garage

--- database/garage/db.js
+++ database/garage/db.js
@@ -36,4 +36,32 @@ const mycars = new Cars ({
     year: 2017
 })
 
+const id = "60be1ba1523df9088c0114c7"
+
+Cars.findById(id, function (err, doc) {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("je suis connecter à la base de donner");
+    }
+})
+
+async function run() {
+    
+    const yearSchema = new mongoose.Schema({
+        year: Number
+    });
+
+    const yearSchema = mongoose.model('year', yearSchema);
+
+    yearSchema.watch().
+    on('change', data => console.log(new Number(), data)) ;
+
+    console.log(new Number(), "j'insert une new doc") ;
+    await yearSchema.create({year: 2000}) ;
+};
+
+
+
+
 mycars.save()
