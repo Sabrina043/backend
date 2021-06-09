8fa91c8e8d910ea67a9e2bce27695935d0ff1e62
sarah <slachehab6@gmail.com>
2021-06-09 17:30:03 +0200

    Hotel

--- chapitre_2/heros/fixture.js
+++ chapitre_2/heros/fixture.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose")
 const bonus_heros = require("bonus_heros")
 
-mongoose.connect("mongodb://localhost:27017/herosDB", (err)=>{
+mongoose.connect("mongodb://localhost:27017/herosDB", (err) => {
 
-    if(err){
+    if (err) {
         console.log(err);
-    }else{
+    } else {
         console.log("je suis connecter à ma database");
     }
 
 })
 
-const addHeros = async () =>{
+const addHeros = async () => {
 
-    try{
-        await bonus_heros.deleteMany ({})
+    try {
+        await bonus_heros.deleteMany({})
 
         await bonus_heros.insertMany([
 
@@ -26,7 +26,7 @@ const addHeros = async () =>{
                 isAlive: true,
                 age: 46,
                 image: "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart"
-            
+
             },
             {
                 name: "Thor",
@@ -35,7 +35,7 @@ const addHeros = async () =>{
                 isAlive: true,
                 age: 300,
                 image: "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg"
-            
+
             },
             {
                 name: "Daredevil",
@@ -44,15 +44,13 @@ const addHeros = async () =>{
                 isAlive: false,
                 age: 30,
                 image: "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg"
-            
+
             }
         ])
         console.log("la collection a était créer dans la database");
 
-    } catch(err) {
-        console.error("Error insertMany heroes: ", err);
+    } catch (err) {
+    } console.error("Error insertMany heroes: ", err);
 }
 
 addHeros()
-
-
